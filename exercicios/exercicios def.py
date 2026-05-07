6059dcde4bba3f9d67c7bd022b9df02473874efb
enzoestanguera <enzo.estanguera@gmail.com>
2026-05-07 11:35:49 -0300

    Aprendendo def

--- exercicios/exercicios def.py
+++ exercicios/exercicios def.py
@@ -4,4 +4,33 @@ def menu():
     print('2- Subtração')
     print('3- Multiplicação')
     print('4- Divisão')
-    numero = int (input("Escolha um numero: "))
+    opcao = int (input("Escolha um numero: "))
+    return opcao
+
+def entrada():
+    print('---entrada---')
+    num = int(input('Numuro: '))
+    return num
+
+def adicao(n1,n2):
+    print('---Adição---')
+    return n1 + n2
+
+def subtracao(n1,n2):
+    print('---Subtração---')
+    return n1 - n2  
+
+def multiplicacao(n1,n2):
+    print('---Multiplicação---')
+    return n1 * n2
+
+def divisao(n1,n2):
+    print('---Divisão---')
+    return n1 / n2
+    
+def imprimir(resultado):
+    print('---Imprimir---')
+    print('================')
+    print(f'Resultado: {resultado}')
+    print('================')
+    
