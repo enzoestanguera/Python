ffdf223b5dfaac4658d7814c1f0d9a4a2bbaa0fc
enzoestanguera <enzo.estanguera@gmail.com>
2026-05-07 11:13:42 -0300

    aprendendo def

---
+++ exercicios/exercicios def.py
@@ -0,0 +1,7 @@
+def menu():
+    print('---Menu---')
+    print('1- Adição')
+    print('2- Subtração')
+    print('3- Multiplicação')
+    print('4- Divisão')
+    numero = int (input("Escolha um numero: "))
