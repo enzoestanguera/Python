c708de38c3cd46c762adf3d076894f49d7d26894
enzoestanguera <enzp.estanguera@gmail.com>
2026-05-08 12:03:21 -0300

    Calculo media

--- exercicios/exercicios alunos.py
+++ exercicios/exercicios alunos.py
@@ -1,26 +1,58 @@
+import os
+
+def valor_errado():
+    os.system('cls')  
+    print('O seu valor esta incorreto!\n')
+
+
 def qtde_alunos():
     print('--Quantidade de Alunos--')
     qtde = 0
-    while qtde <1:
+
+    while qtde < 1:
         qtde = int(input('Qtde de alunos: '))
+
         if qtde < 1:
-            print('ESta quantidade tem que ser MAIOR que ZERO')
-        return qtde
+            valor_errado()
+
+    return qtde
 
 def preencher_notas(qtde):
-    print('--Preeenchendo as notas--')
+    print('--Preenchendo as notas--')
     notas = []
+
     for i in range(qtde):
         print(f'Aluno {i+1}: ')
         nota = float(input('Nota: '))
         notas.append(nota)
     return notas
 
-def imprimir_notas(qtde_alunos, qtde):
-    print('--Imprimindo as notas dos alunos... ---')
+def imprimir_notas(notas):
+    print('--Imprimindo as notas dos alunos---')
 
-import os
-def valor_errado():
-    os.system('cls')
-    print('O seu valor esta incorreto!')
-    
+    i = 1
+    for nota in notas:
+        print(f'Aluno: {i}')
+        print(f'Nota: {nota}')
+        i += 1
+
+def calcula_media(lista_notas):
+    soma=0
+    for nota in lista_notas:
+        soma+=nota
+    media = soma/ len(lista_notas)
+    return media
+   
+
+def notas_baixas(lista_notas):
+    media = calcula_media(lista_notas)
+    cont = 0
+    for nota in lista_notas:
+        if nota < media:
+            cont+=1
+
+# principal
+quantidade = qtde_alunos()
+lista_notas = preencher_notas(quantidade)
+media = calcula_media
+imprimir_notas(lista_notas)
